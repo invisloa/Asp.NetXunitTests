75d61bc5fd6aebba50c99ea4abd35b94e3b980bb
invisloa <91217120+invisloa@users.noreply.github.com>
2023-03-22 17:33:29 +0100

    Add project files.

---
+++ SimpleApp.Tests/ProductTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using SimpleApp.Models;
+using Xunit;
+
+namespace SimpleApp.Tests
+{
+	public class ProductTests
+	{ 
+	[Fact]
+	public void CanChangeProductName()
+		{ 
+		
+	// Arrange
+	var p = new Product { Name = "Test", Price = 100M };
+		// Act
+		p.Name = "New Name";
+		//Assert
+		Assert.Equal("New Name", p.Name);
+	}
+	[Fact]
+	public void CanChangeProductPrice()
+	{
+		// Arrange
+		var p = new Product { Name = "Test", Price = 100M };
+		// Act
+		p.Price = 200M;
+		//Assert
+		Assert.Equal(100M, p.Price);
+	}
+}
+}
